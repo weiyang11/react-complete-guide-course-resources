2e06910c141247fdcc68a94a9c667d9c2839a9a9
weiyang <weiyang_11@outlook.com>
2024-07-15 17:47:26 +0800

    lift up state

--- code/05 Essentials Practice/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,11 +1,36 @@
+import React from 'react';
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Result from './components/Result.jsx';
 
 function App() {
+
+
+  const [userInput, setUserInput] = React.useState(
+    {
+      initialInvestment: 10000,
+      annualInvestment: 1200,
+      expectedReturn: 6,
+      duration:10,
+    }
+  );
+
+
+  function handleChange(InputIndentifier, NewValue){
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [InputIndentifier]: NewValue
+      }
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Result input={userInput} />
+
     </>
   );
 }
---
+++ code/05 Essentials Practice/01-starting-project/src/components/Result.jsx
@@ -0,0 +1,5 @@
+export default function Result({ input }) {
+  return (
+    <p>Result</p>
+  )
+}
