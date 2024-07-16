88ded39095ab852a3d9ad7b6f0411436773f2fac
weiyang <weiyang_11@outlook.com>
2024-07-16 18:58:03 +0800

    reusable style component and combination

--- code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
+++ code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
@@ -30,6 +30,21 @@ const Input = styled.input`
 }
 `
 
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+  background-color: #f0920e;
+}
+`
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -80,7 +95,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button  onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
