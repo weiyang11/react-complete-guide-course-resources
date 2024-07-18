85564dc6c94dac548837bc55276db4b01fa06ad3
weiyang <weiyang_11@outlook.com>
2024-07-18 14:54:41 +0800

    migrate all the valina css to tailwind css

--- code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
+++ code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
+// import { styled } from 'styled-components';
 import Button from './Button.jsx';
 import Input from './Input.jsx';
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-` // styled component
+// const ControlContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// ` // styled component
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -31,9 +31,10 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs"
+    className= "w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-500 to-stone-800">
 
+      <div className='flex flex-col gap-2 mb-6'>
           <Input
             label="Email"
             type="email"
@@ -50,9 +51,9 @@ export default function AuthInputs() {
             }
           />
 
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button  onClick={handleLogin}>Sign In</Button>
