2cee86ca172037378620325f5755c2c95920fe56
weiyang <weiyang_11@outlook.com>
2024-07-22 15:52:45 +0800

    apply ref for button click and get input value

--- code/08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,28 @@
+import React from 'react';
+
 export default function Player() {
+
+  const playerName = React.useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = React.useState(null);
+  // const [submitted, setSubmitted] = React.useState(false);
+
+  // function handleChanhge(event) {
+  //   setEnteredPlayerName(event.target.value);
+  //   setSubmitted(false);
+  // }
+
+  function handleClick() {
+    // setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
