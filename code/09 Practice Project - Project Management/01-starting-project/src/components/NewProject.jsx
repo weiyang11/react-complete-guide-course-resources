6619a27d90f4ce38eaa23c946adc1e75942c47b7
weiyang <weiyang_11@outlook.com>
2024-07-23 18:01:13 +0800

    handling project creation and updating the ui

--- code/09 Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
@@ -1,16 +1,34 @@
+import React from 'react'
 import Input from './Input'
 
-export default function NewProject() {
+export default function NewProject({onAdd}) {
+  const title = React.useRef();
+  const description = React.useRef();
+  const dueDate = React.useRef();
+
+  function handleSave(){
+    const entedTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+    //validation...
+    onAdd({
+      title: entedTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate
+    })
+
+  }
+
   return (
     <div className= "w-[35rem] mt-16">
       <menu className = "flex items-center justify-end gap-4 my-4">
         <li><button className = "text-stone-800 hover:text-stone-950">Cancel</button></li>
-        <li><button className = " px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+        <li><button className = " px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>Save</button></li>
       </menu>
       <div>
-        <Input label='Title' />
-        <Input label='Description' textarea />
-        <Input label='Due Date' />
+        <Input type="text" label='Title' ref={title}/>
+        <Input label='Description' textarea ref={description}/>
+        <Input type ="date" label='Due Date' ref={dueDate} />
       </div>
     </div>
   )
