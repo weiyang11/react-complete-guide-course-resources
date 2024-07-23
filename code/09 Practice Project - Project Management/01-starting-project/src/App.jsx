a715b2d5f2a135f75d8061b542191a70970444be
weiyang <weiyang_11@outlook.com>
2024-07-23 17:29:47 +0800

    add the reat use state to define the different page within home page once click button

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -1,12 +1,37 @@
+import React from "react";
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectSideBar";
 
 function App() {
+
+  const [projectsState, setProjectsState] = React.useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null, //adding new project
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className= "h-screen my-8  flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar  onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
