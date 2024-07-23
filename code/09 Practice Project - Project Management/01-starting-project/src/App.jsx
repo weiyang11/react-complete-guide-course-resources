37651005c9753f16ac143c516e13052014f1a222
weiyang <weiyang_11@outlook.com>
2024-07-23 19:21:01 +0800

    add the delete function

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -57,9 +57,19 @@ function App() {
     });
   }
 
+  function handleDeleteProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId) //removing the selected project
+      };
+    });
+  }
+
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content =<SelectedProject project={selectedProject} />;
+  let content =<SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd= {handleAddProject} onCancel={handleCancelAddProject}/>;
