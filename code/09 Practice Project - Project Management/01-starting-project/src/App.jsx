de2920d374ce4410acb69fd5151223d764777032
weiyang <weiyang_11@outlook.com>
2024-07-23 20:02:56 +0800

    add task by managing tasks and prop drilling

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -9,8 +9,30 @@ function App() {
 
   const [projectsState, setProjectsState] = React.useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
+
+  function handleAddTask(text){
+    setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return{
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks]
+      };
+    });
+
+  }
+
+  function handleDeleteTask(){
+
+  }
 
   function handleSelectProject(id){
     setProjectsState(prevState => {
@@ -69,7 +91,7 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content =<SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  let content =<SelectedProject project={selectedProject} onDelete={handleDeleteProject} onAddTask={handleAddTask} onDeleteTask={handleDeleteTask} tasks = {projectsState.tasks}/>;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd= {handleAddProject} onCancel={handleCancelAddProject}/>;
