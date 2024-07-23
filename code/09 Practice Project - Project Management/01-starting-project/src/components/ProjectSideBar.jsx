b86fe70b05e89d6e69ecf3e8cbed4da05fe6620d
weiyang <weiyang_11@outlook.com>
2024-07-23 16:50:03 +0800

    input component and new project created and add some tailwind to components

--- code/09 Practice Project - Project Management/01-starting-project/src/components/ProjectSideBar.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/components/ProjectSideBar.jsx
@@ -4,7 +4,7 @@ export default function ProjectsSidebar(){
       <h2 className = "mb-8 font-bold uppercase md: text-xl text-stone-200">Your Projects</h2>
       <div>
         <button className = "px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-          Add Project
+          + Add Project
         </button>
         <ul>
 
